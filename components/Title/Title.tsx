e4b98284baa5a8c5c1938c20df8cd685e9b7f017
akibqureshee11@gmail.com <akibqureshee11@gmail.com>
2026-02-05 15:05:51 +0600

    refactor: update styles and add social icons to Banner component

--- components/Title/Title.tsx
+++ components/Title/Title.tsx
@@ -15,7 +15,7 @@ export default function Title({ children }: TitleProps): React.ReactElement {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
     >
-      <h1 className="text-2xl xs:text-3xl sm:text-4xl md:text-5xl font-medium uppercase text-white before:content-[''] before:block before:w-[82px] before:h-2 before:rounded-[2rem] before:bg-[#2282ff] before:mb-2">
+      <h1 className="text-[6vw] sm:text-4xl md:text-5xl 3xl:text-6xl font-medium uppercase text-white before:content-[''] before:block before:w-20.5 before:h-2 before:rounded-4xl font-auto_wide before:bg-[#2282ff] before:mb-2 text-center sm:text-left">
         {children}
       </h1>
     </motion.div>
