0804ad415469dec59c6a820d5596739db2b6b5f6
akibqureshee11@gmail.com <akibqureshee11@gmail.com>
2026-02-10 18:12:20 +0600

    updated skills section

--- components/Banner/ScrollDown.tsx
+++ components/Banner/ScrollDown.tsx
@@ -19,9 +19,9 @@ export default function ScrollDown(): React.ReactElement {
       aria-label="Scroll to next section"
       className="absolute bottom-1 left-1/2 -translate-x-1/2 lg:flex cursor-pointer flex-col items-center justify-center opacity-80 transition-opacity hover:opacity-100 hidden"
     >
-      <div className="relative h-[60px] w-[30px] rounded-[60px] border-[3px] border-white">
+      <div className="relative h-15 w-7.5 rounded-2xl border-2 border-white">
         <div
-          className={`absolute left-1/2 top-[5px] h-[8px] w-[8px] -translate-x-1/2 rounded-full bg-white ${styles.mouseWheel}`}
+          className={`absolute left-1/2 top-1.5 h-2.5 w-2.5 -translate-x-1/2 rounded-full bg-white ${styles.mouseWheel}`}
         />
       </div>
     </button>
