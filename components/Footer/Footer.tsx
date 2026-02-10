17ed77ae10c17f5f87caaf42246e9a67c93a1547
akibqureshee11@gmail.com <akibqureshee11@gmail.com>
2026-02-10 18:18:33 +0600

    updated skills section

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -8,9 +8,9 @@ export default function Footer(): React.ReactElement {
   return (
     <footer className="hero mt-12 sm:mt-16 lg:mt-24 backdrop-blur-md bg-black/20 py-3 sm:py-4 px-4 sm:px-6 lg:px-8">
       <div className="flex justify-center sm:justify-between flex-wrap gap-4 sm:gap-0 text-white/40 items-center max-w-7xl mx-auto">
-        <div className="tracking-wider sm:tracking-widest text-center text-xs sm:text-sm 3xl:text-base">
+        <div className="tracking-wider sm:tracking-widest w-fit mx-auto text-center text-xs sm:text-sm 3xl:text-base">
           <FaRegCopyright className="inline mb-0.5 sm:mb-1" /> All rights reserved by Ishak
-          Qureshee Akib {currentYear}
+          Qureshee Akib- {currentYear}
         </div>
       </div>
     </footer>
