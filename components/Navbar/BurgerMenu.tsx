58dd71697af8ae333081a2d8cbe9cdfc96777947
akibqureshee11@gmail.com <akibqureshee11@gmail.com>
2026-02-10 23:26:24 +0600

    updated skills section

--- components/Navbar/BurgerMenu.tsx
+++ components/Navbar/BurgerMenu.tsx
@@ -108,10 +108,6 @@ export default function BurgerMenu(): React.ReactElement | null {
             ))}
           </ul>
         </div>
-        
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0 pointer-events-none select-none">
-           <span className="text-[20vw] font-bold text-white/5 whitespace-nowrap">MENU</span>
-        </div>
       </div>
     </div>,
     document.body
