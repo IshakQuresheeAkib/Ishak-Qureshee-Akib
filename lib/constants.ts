30e48a6fe01d5300db5fe8cac2f1c69be97ebf2c
akibqureshee11@gmail.com <akibqureshee11@gmail.com>
2026-02-27 05:25:07 +0600

    update resume link in external URLs

--- lib/constants.ts
+++ lib/constants.ts
@@ -15,7 +15,7 @@ export const SOCIAL_LINKS = {
 } as const;
 
 export const EXTERNAL_URLS = {
-  resume: "https://www.dropbox.com/scl/fi/a6czqdsc6oxtmzxfj05yq/Resume-of-Akib-1.pdf?rlkey=1nbo4mpf4u24jp2z28r6d9z1s&st=rzdv6dcq&dl=1",
+  resume: "https://www.dropbox.com/scl/fi/d1qsesy0ppqsd50zxmcqa/Resume-of-Akib.pdf?rlkey=7evxodrqybc4mhn8p7xa1o4dt&st=e5k7g352&dl=1",
   avatarImage: "https://i.ibb.co/Z6PYFGCp/Copy-of-Untitled-Design.png",
   aboutImage: "https://i.ibb.co/hJbS7G9G/Untitled-design-1.png",
 } as const;
